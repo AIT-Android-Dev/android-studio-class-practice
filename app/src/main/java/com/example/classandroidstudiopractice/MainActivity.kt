3ecc5c4139f46933afbe03cc95396a315b89702f
Emir <148708322+eeemmm29@users.noreply.github.com>
2026-01-26 11:07:37 +0600

    Class3.1

--- app/src/main/java/com/example/classandroidstudiopractice/MainActivity.kt
+++ app/src/main/java/com/example/classandroidstudiopractice/MainActivity.kt
@@ -2,15 +2,18 @@ package com.example.classandroidstudiopractice
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -19,7 +22,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -32,50 +38,67 @@ class MainActivity : ComponentActivity() {
         setContent {
             ClassAndroidStudioPracticeTheme {
                 Greeting(
+                    message = stringResource(R.string.happy_birthday),
+                    from = stringResource(R.string.from_text),
+                    modifier = Modifier.padding(8.dp)
                 )
             }
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("MainActivity", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("MainActivity", "onResume")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("MainActivity", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MainActivity", "onDestroy")
+    }
 }
 
 @Composable
-fun Greeting() {
-
-    Log.d("Demo", "Recomposed")
+fun Greeting(message: String, from: String, modifier: Modifier = Modifier) {
 
     var count by remember { mutableIntStateOf(0) }
-    var name by remember { mutableStateOf("") }
 
     Column(
-        horizontalAlignment = Alignment.CenterHorizontally
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
     ) {
         Text(
-            "Count: $count", modifier =
-                Modifier
-                    .padding(16.dp)
-                    .background(Color.Red)
-                    .padding(16.dp)
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 110.sp,
+            textAlign = TextAlign.Center
         )
-
         Text(
-            if (count < 5) "Low" else "High",
-            color = Color.Blue,
-            fontSize = 24.sp
+            text = from,
+            fontSize = 30.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.CenterHorizontally)
         )
 
-        Button(onClick = {
-            count++
-        }) { Text("Count") }
-
-        TextField(
-            value = name,
-            onValueChange = { text ->
-                name = text
-            },
-            label = { Text("Name") }
+        Button(
+            onClick = { count++ }
+        ) {}
+        Text(
+            text = count.toString(),
+            fontSize = 100.sp,
+            lineHeight = 110.sp,
         )
-
-        Text("Hello, $name")
     }
 }
 
@@ -83,6 +106,28 @@ fun Greeting() {
 @Composable
 fun GreetingPreview() {
     ClassAndroidStudioPracticeTheme {
-        Greeting()
+        GreetingImage(
+            message = "Happy Birthday",
+            from = "From Emir",
+            modifier = Modifier.padding(8.dp)
+        )
+    }
+}
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier) {
+    val image = painterResource(R.drawable.androidparty__1_)
+
+    Box(modifier) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5f
+        )
+        Greeting(
+            message,
+            from
+        )
     }
 }
