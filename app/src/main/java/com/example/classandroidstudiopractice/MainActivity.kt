2efb93b70a23ee46fe27033c01e8fc2fc0c22b19
Emir <148708322+eeemmm29@users.noreply.github.com>
2026-01-28 14:03:28 +0600

    Class 3.2

--- app/src/main/java/com/example/classandroidstudiopractice/MainActivity.kt
+++ app/src/main/java/com/example/classandroidstudiopractice/MainActivity.kt
@@ -2,29 +2,28 @@ package com.example.classandroidstudiopractice
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,35 +34,49 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        log("onCreate")
+
         setContent {
             ClassAndroidStudioPracticeTheme {
-                Greeting(
-                    message = stringResource(R.string.happy_birthday),
-                    from = stringResource(R.string.from_text),
-                    modifier = Modifier.padding(8.dp)
+//                Greeting(
+//                    message = stringResource(R.string.happy_birthday),
+//                    from = stringResource(R.string.from_text),
+//                    modifier = Modifier.padding(8.dp)
+//                )
+                LogsList(
+                    logs,
+                    Modifier.padding(8.dp)
                 )
             }
         }
     }
 
+    val logs = mutableStateListOf<String>()
+
     override fun onStart() {
         super.onStart()
-        Log.d("MainActivity", "onStart")
+        log("onStart")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d("MainActivity", "onResume")
+        log("onResume")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.d("MainActivity", "onStop")
+        log("onStop")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d("MainActivity", "onDestroy")
+        log("onDestroy")
+    }
+    
+    fun log(message: String) {
+        Log.d("MainActivity", message)
+        logs.add(message)
     }
 }
 
@@ -91,13 +104,9 @@ fun Greeting(message: String, from: String, modifier: Modifier = Modifier) {
                 .align(alignment = Alignment.CenterHorizontally)
         )
 
-        Button(
-            onClick = { count++ }
-        ) {}
-        Text(
-            text = count.toString(),
-            fontSize = 100.sp,
-            lineHeight = 110.sp,
+        Icon(
+            imageVector = Icons.Filled.Call,
+            contentDescription = null,
         )
     }
 }
@@ -106,28 +115,23 @@ fun Greeting(message: String, from: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     ClassAndroidStudioPracticeTheme {
-        GreetingImage(
+        Greeting(
             message = "Happy Birthday",
             from = "From Emir",
             modifier = Modifier.padding(8.dp)
         )
+//        LogsList(
+//            logs = listOf("Log 1", "Log 2", "Log 3"),
+//            modifier = Modifier.padding(8.dp)
+//        )
     }
 }
 
 @Composable
-fun GreetingImage(message: String, from: String, modifier: Modifier) {
-    val image = painterResource(R.drawable.androidparty__1_)
-
-    Box(modifier) {
-        Image(
-            painter = image,
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            alpha = 0.5f
-        )
-        Greeting(
-            message,
-            from
-        )
+fun LogsList(logs: List<String>, modifier: Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(logs) { log ->
+            Text(text = log)
+        }
     }
 }
